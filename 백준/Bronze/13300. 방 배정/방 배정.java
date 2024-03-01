b478e16749a41bee86e9cfc11fcf936836b82ef4
NAKTA <nakta.y.biz@gmail.com>
2024-03-01 21:18:18 +0900

    [Bronze II] Title: 방 배정, Time: 200 ms, Memory: 15144 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -4,31 +4,31 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int answer = 0;
-            
+
         String[] countAndCapacity = br.readLine().split(" ");
         int n = Integer.parseInt(countAndCapacity[0]);
         int capacity = Integer.parseInt(countAndCapacity[1]);
-        
+
         int[] manRooms = new int[7];
         int[] girlRooms = new int[7];
-        
+
         for (int i = 0; i < n; i++) {
             String[] gradeAndCount = br.readLine().split(" ");
             int gender = Integer.parseInt(gradeAndCount[0]);
             int grade = Integer.parseInt(gradeAndCount[1]);
-            
+
             if (gender == 0) {
-                manRooms[grade]++; 
-            } else {
                 girlRooms[grade]++;
+            } else {
+                manRooms[grade]++;
             }
         }
-        
+
         for (int i = 1; i < 7; i++) {
-            answer += (int) Math.ceil(manRooms[i] / 2.0);
-            answer += (int) Math.ceil(girlRooms[i] / 2.0);
+            answer += (int) Math.ceil((double) manRooms[i] / capacity);
+            answer += (int) Math.ceil((double) girlRooms[i] / capacity);
         }
-        
+
         System.out.println(answer);
     }
 }
