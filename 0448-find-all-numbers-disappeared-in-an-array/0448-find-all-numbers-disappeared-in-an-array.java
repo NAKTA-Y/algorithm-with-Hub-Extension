cc878cd251a22c5f818848e2c3a882b7f9ff6bdd
NAKTA <nakta.y.biz@gmail.com>
2024-08-11 10:41:13 +0900

    Time: 12 ms (26.62%), Space: 53.2 MB (91.58%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,18 +1,14 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-
         for (int num : nums) {
             int index = Math.abs(num);
             if (nums[index-1] > 0)
                 nums[index-1] *= -1;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) 
-                result.add(i+1);
-        }
-
-        return result;
+        return IntStream.range(0, nums.length)
+                .filter(idx -> nums[idx] > 0)
+                .map(idx -> idx + 1).boxed()
+                .toList();
     }
 }
