8ce640e5f877ff4b339ec2dabf0414b7eaf8497d
NAKTA <nakta.y.biz@gmail.com>
2024-08-12 10:17:56 +0900

    Time: 0 ms (100%), Space: 44.4 MB (45.68%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,13 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow_pointer = head, fast_pointer = head;
-        while (fast_pointer != null && fast_pointer.next != null) {
-            slow_pointer = slow_pointer.next;
-            fast_pointer = fast_pointer.next.next;
-            if (slow_pointer == fast_pointer) {
-                return true;
-            }
+        ListNode turtle = head, rabbit = head;
+
+        while (rabbit != null && rabbit.next != null) {
+            turtle = turtle.next;
+            rabbit = rabbit.next.next;
+
+            if (turtle == rabbit) return true;
         }
+
         return false;
     }
 }
