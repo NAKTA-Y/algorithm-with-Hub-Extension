65af877e9d1bbaa5737c3371fc4d4f4d9f2b3c77
NAKTA <nakta.y.biz@gmail.com>
2024-08-13 12:47:44 +0900

    Time: 70 ms (45.1%), Space: 62.5 MB (42.96%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,32 +1,14 @@
-class Temp {
-    int temp;
-    int index;
-
-    Temp (int temp, int index) {
-        this.temp = temp;
-        this.index = index;
-    }
-}
-
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Temp> stack = new Stack<>();
-
+        Stack<Integer> stack = new Stack<>();
         int len = temperatures.length;
         int[] answer = new int[len];
 
-        for (int i = len-1; i >= 0; i--) {
-            int days = 0;
-            
-            while (!stack.isEmpty() && stack.peek().temp <= temperatures[i]) {
-                Temp pop = stack.pop();
-
-                days += answer[pop.index];
+        for (int i = 0; i < len; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                answer[stack.peek()] = i - stack.pop();
             }
-
-            answer[i] = stack.isEmpty() ? 0 : days + 1;
-
-            stack.push(new Temp(temperatures[i], i));
+            stack.push(i);
         }
 
         return answer;
