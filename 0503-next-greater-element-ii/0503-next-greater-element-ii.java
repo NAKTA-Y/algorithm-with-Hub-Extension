72e952c737f2af70f9b4d4997e5f4c4167fc7ad9
NAKTA <nakta.y.biz@gmail.com>
2024-08-13 12:05:34 +0900

    Time: 14 ms (78.33%), Space: 45.7 MB (93.14%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
         int len = nums.length;
-        int[] answer = new int[len];
 
-        int index = len-1;
-        for (int i = 0; i < len * 2; i++) {
-            while (!stack.isEmpty() && stack.peek() <= nums[index]) stack.pop();
-
-            if (!stack.isEmpty() && !map.containsKey(nums[index])) {
-                answer[index] = stack.peek();
-            } else {
-                answer[index] = -1;
-            }
-
-            stack.push(nums[index]);
-
-            index = index == 0 ? len-1 : index-1;
+        for (int i = len-1; i >= 0; i--) {
+            stack.push(nums[i]);
         }
 
-        return answer;
+        for (int i = len-1; i >= 0; i--) {
+            int num = nums[i];
+            while (!stack.isEmpty() && stack.peek() <= nums[i]) {
+                stack.pop();
+            }
+
+            nums[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(num);
+        }
+
+        return nums;
     }
 }
