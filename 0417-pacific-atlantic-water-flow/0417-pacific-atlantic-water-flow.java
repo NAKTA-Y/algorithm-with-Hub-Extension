ec8878c6507a090911ea5ca6f1862930c677c92e
NAKTA <nakta.y.biz@gmail.com>
2024-08-17 15:13:14 +0900

    Time: 4 ms (97.28%), Space: 45.2 MB (87.92%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,89 +1,38 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
-
 class Solution {
-    private Deque<int[]> queue = new ArrayDeque<>();
-    int[][] directions = {{0,1}, {1,0}, {-1,0}, {0,-1}};
-
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        List<List<Integer>> result = new ArrayList<>();
-        int height = heights.length;
-        int width = heights[0].length;
+        int rows = heights.length, cols = heights[0].length;
+        boolean[][] pac = new boolean[rows][cols];
+        boolean[][] atl = new boolean[rows][cols];
 
-        boolean[][] canPacific = new boolean[height][width];
-        boolean[][] canAtlantic = new boolean[height][width];
-
-        // pacific
-        for (int i = 0; i < width; i++) {
-            if (canPacific[0][i]) continue;
-            canPacific[0][i] = true;
-            queue.add(new int[] {0, i});
-            bfs(heights, canPacific);
+        for (int col = 0; col< cols; col++){
+            dfs(0, col, rows, cols, pac, heights[0][col], heights);
+            dfs(rows-1, col,rows, cols, atl, heights[rows-1][col], heights);
         }
-
-        for (int i = 0; i < height; i++) {
-            if (canPacific[i][0]) continue;
-            canPacific[i][0] = true;
-            queue.add(new int[] {i, 0});
-            bfs(heights, canPacific);
+        for (int row = 0; row<rows; row++){
+            dfs(row, 0,rows, cols, pac, heights[row][0], heights);
+            dfs(row, cols-1,rows, cols, atl, heights[row][cols-1], heights);
         }
-
-        // atlantic
-        for (int i = 0; i < width; i++) {
-            if (canAtlantic[height-1][i]) continue;
-            canAtlantic[height-1][i] = true;
-            queue.add(new int[] {height-1, i});
-            bfs(heights, canAtlantic);
-        }
-
-        for (int i = 0; i < height; i++) {
-            if (canAtlantic[i][width-1]) continue;
-            canAtlantic[i][width-1] = true;
-            queue.add(new int[] {i, width-1});
-            bfs(heights, canAtlantic);
-        }
-
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (canPacific[i][j] && canAtlantic[i][j]) {
-                    List<Integer> both = new ArrayList<>();
-                    both.add(i);
-                    both.add(j);
-                    result.add(both);
-                }
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++){
+                if (pac[i][j] && atl[i][j])
+                    result.add(Arrays.asList(i,j));
             }
-        }
-
         return result;
     }
 
-    public void bfs(int[][] heights, boolean[][] canOcean) {
-        int x = 0;
-        int y = 0;
-        int height = heights.length;
-        int width = heights[0].length;
+    private void dfs(int row, int col, int rows, int cols, boolean[][] visited, int prevHeight, int[][] heights){
+        // 범위를 벗어나거나, 이미 방문했을 경우, 이전 높이 기준으로 더 클 경우
+        if (row < 0 || row >= rows || col < 0 || col >= cols || visited[row][col] || prevHeight > heights[row][col])
+            return;
 
-        while (!queue.isEmpty()) {
-            int[] pos = queue.poll();
+        // 방문 처리
+        visited[row][col]= true;
 
-            x = pos[0];
-            y = pos[1];
+        // 다음 포인트 이동
+        dfs(row+1, col, rows, cols, visited, heights[row][col], heights);
+        dfs(row-1, col, rows, cols, visited, heights[row][col], heights);
+        dfs(row, col+1, rows, cols, visited, heights[row][col], heights);
+        dfs(row, col-1, rows, cols, visited, heights[row][col], heights);
 
-            for (int i = 0; i < 4; i++) {
-                int moveX = x + directions[i][0];
-                int moveY = y + directions[i][1];
-
-                // out of range
-                if (moveX < 0 || moveY < 0 || moveX >= height || moveY >= width) continue;
-
-                // can overflow
-                if (heights[moveX][moveY] >= heights[x][y] && !canOcean[moveX][moveY]) {
-                    queue.add(new int[]{moveX, moveY});
-                    canOcean[moveX][moveY] = true;
-                }
-            }
-        }
-    }
-}
+    }}
