975f1f106f7e29a086c43fb4d8a09f5937d2cb95
NAKTA <nakta.y.biz@gmail.com>
2024-08-19 16:26:04 +0900

    Time: 6 ms (61.46%), Space: 46 MB (78.56%) - LeetHub

---
+++ 1254-deepest-leaves-sum/1254-deepest-leaves-sum.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        int sum = 0;
+        
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            sum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+        }
+
+        return sum;
+    }
+}
