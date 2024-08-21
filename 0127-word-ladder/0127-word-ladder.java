bd52ebb05c1976a147f48c4a87ca552ef5247533
NAKTA <nakta.y.biz@gmail.com>
2024-08-21 20:18:25 +0900

    Time: 260 ms (21.82%), Space: 47.7 MB (30.44%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,57 +1,37 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        wordList.add(beginWord);
-        int size = wordList.size();
-
-        Map<String, List<String>> wordMap = new HashMap<>();
-        for (String word : wordList) {
-            wordMap.put(word, new ArrayList<>());
-        }
-
-        for (int i = 0; i < size-1; i++) {
-            for (int j = i+1; j < size; j++) {
-                String source = wordList.get(i);
-                String target = wordList.get(j);
-                if (canTransformByOneChar(source, target)) {
-                    wordMap.get(source).add(target);
-                    wordMap.get(target).add(source);
-                }
-            }
-        }
-
-        Set<String> visited = new HashSet<>();
-        Deque<String> queue = new ArrayDeque<>();
-        int count = 1;
-
+        Set<String> set = new HashSet<>(wordList);
+        if(!set.contains(endWord)) return 0;
+        
+        Queue<String> queue = new LinkedList<>();
         queue.add(beginWord);
-        while (!queue.isEmpty()) {
-            int queuesize = queue.size();
-
-            for (int i = 0; i < queuesize; i++) {
+        
+        Set<String> visited = new HashSet<>();
+        queue.add(beginWord);
+        
+        int changes = 1;
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
                 String word = queue.poll();
-                if (word.equals(endWord)) return count;
-
-                List<String> possibleWords = wordMap.get(word);
-                for (String possibleWord : possibleWords) {
-                    if (visited.contains(possibleWord)) continue;
-                    visited.add(possibleWord);
-                    queue.add(possibleWord);
+                if(word.equals(endWord)) return changes;
+                
+                for(int j = 0; j < word.length(); j++){
+                    for(int k = 'a'; k <= 'z'; k++){
+                        char arr[] = word.toCharArray();
+                        arr[j] = (char) k;
+                        
+                        String str = new String(arr);
+                        if(set.contains(str) && !visited.contains(str)){
+                            queue.add(str);
+                            visited.add(str);
+                        }
+                    }
                 }
             }
-
-            count++;
+            ++changes;
         }
-
         return 0;
-    }
-
-    private boolean canTransformByOneChar(String source, String target) {
-        int count = 0;
-
-        for (int i = 0; i < source.length(); i++) {
-            if (source.charAt(i) != target.charAt(i)) count++;
-        }
-
-        return count == 1;
     }
 }
