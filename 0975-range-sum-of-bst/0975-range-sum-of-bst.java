625ca0042929b04efb1fa57dd8d37c618481a35e
NAKTA <nakta.y.biz@gmail.com>
2024-08-21 18:52:40 +0900

    Time: 1 ms (50.93%), Space: 50.8 MB (86.74%) - LeetHub

---
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        dfs(root, low, high);
+        return sum;
+    }
+
+    public void dfs(TreeNode root, int low, int high) {
+        if (root == null) return;
+        if (low <= root.val && high >= root.val) sum += root.val;
+
+        dfs(root.left, low, high);
+        dfs(root.right, low, high);
+    }
+}
