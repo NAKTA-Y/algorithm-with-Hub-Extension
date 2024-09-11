54316a8a7a3ef441a7ec77fe49f0aaffcff6f130
NAKTA <nakta.y.biz@gmail.com>
2024-09-11 11:08:21 +0900

    Time: 25 ms (76.05%), Space: 42.4 MB (89.27%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -34,13 +34,11 @@ class Solution {
             Node copy = nodeMap.get(root.val);
 
             for (Node connectNode : root.neighbors) {
-                Node newNode;
+                Node newNode = nodeMap.getOrDefault(connectNode.val, new Node(connectNode.val));
+                
                 if (!nodeMap.containsKey(connectNode.val)) {
-                    newNode = new Node(connectNode.val);
                     nodeMap.put(newNode.val, newNode);
                     queue.add(connectNode);
-                } else {
-                    newNode = nodeMap.get(connectNode.val);
                 }
 
                 copy.neighbors.add(newNode);
