3af5154616c400664f08273f36b36205eca30ebe
NAKTA <nakta.y.biz@gmail.com>
2024-09-11 11:05:35 +0900

    Time: 28 ms (18.16%), Space: 43 MB (8.49%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,52 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        Map<Integer, Node> nodeMap = new HashMap<>();
+        Deque<Node> queue = new ArrayDeque<>();
+
+        if (node == null) return null;
+
+        Node copyRoot = new Node(1);
+        nodeMap.put(1, copyRoot);
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            Node root = queue.poll();
+            Node copy = nodeMap.get(root.val);
+
+            for (Node connectNode : root.neighbors) {
+                Node newNode;
+                if (!nodeMap.containsKey(connectNode.val)) {
+                    newNode = new Node(connectNode.val);
+                    nodeMap.put(newNode.val, newNode);
+                    queue.add(connectNode);
+                } else {
+                    newNode = nodeMap.get(connectNode.val);
+                }
+
+                copy.neighbors.add(newNode);
+            }
+        }
+
+        return copyRoot;
+    }
+}
