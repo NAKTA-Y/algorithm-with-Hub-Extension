7290ee1b2f04f661d545c0e6a67008a876a00158
NAKTA <nakta0511@gmail.com>
2024-09-22 21:10:26 +0900

    [Silver I] Title: 쿼드트리, Time: 104 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.kt
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.kt
@@ -11,59 +11,49 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
         screen[i] = row
     }
 
-    if (isNotMatched(0, 0, n, screen))
-        compress(0, 0, n, screen)
-    else compression.append(screen[0][0])
+    compress(0, 0, n, screen)
 
     println(compression)
 }
 
 fun compress(startX: Int, startY: Int, length: Int, screen: Array<IntArray>) {
+
+    if (isMatched(startX, startY, length, screen)) {
+        compression.append(screen[startX][startY])
+        return
+    }
+
     // 전체 범위 검사
     compression.append('(')
 
-    if (isNotMatched(startX, startY, length, screen)) {
-        // 나눠서 검사
-        val half = length / 2
+    val half = length / 2
 
-        // 1분면
-        if (isNotMatched(startX, startY, half, screen))
-            compress(startX, startY, half, screen)
-        else compression.append(screen[startX][startY])
+    // 1분면
+    compress(startX, startY, half, screen)
 
-        // 2분면
-        if (isNotMatched(startX, startY + half, half, screen))
-            compress(startX, startY + half, half, screen)
-        else compression.append(screen[startX][startY + half])
+    // 2분면
+    compress(startX, startY + half, half, screen)
 
-        // 3분면
-        if (isNotMatched(startX + half, startY, half, screen))
-            compress(startX + half, startY, half, screen)
-        else compression.append(screen[startX + half][startY])
+    // 3분면
+    compress(startX + half, startY, half, screen)
 
-        // 4분면
-        if (isNotMatched(startX + half, startY + half, half, screen))
-            compress(startX + half, startY + half, half, screen)
-        else compression.append(screen[startX + half][startY + half])
-
-    } else {
-        compression.append(screen[startX][startY])
-    }
+    // 4분면
+    compress(startX + half, startY + half, half, screen)
 
     compression.append(')')
 }
 
-fun isNotMatched(startX: Int,
-                 startY: Int,
-                 half: Int,
-                 paper: Array<IntArray>): Boolean {
+fun isMatched(startX: Int,
+              startY: Int,
+              half: Int,
+              paper: Array<IntArray>): Boolean {
 
     val current = paper[startX][startY]
     for (i in startX until startX + half) {
         for (j in startY until startY + half) {
-            if (current != paper[i][j]) return true
+            if (current != paper[i][j]) return false
         }
     }
 
-    return false
+    return true
 }
