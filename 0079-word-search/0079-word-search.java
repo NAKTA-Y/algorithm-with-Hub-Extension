bde9630487cdc29be7f64a12f96d82351efba089
NAKTA <nakta0511@gmail.com>
2024-09-23 20:02:34 +0900

    Time: 136 ms (73.98%), Space: 41.6 MB (50.44%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -8,15 +8,10 @@ class Solution {
         width = board[0].length;
         char[] charArray = word.toCharArray();
 
-        return findWord(board, charArray, word);
-    }
-
-    public boolean findWord(char[][] board, char[] charArray, String word) {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 if (board[i][j] == charArray[0]) {
-                    backtracking(i, j, 0, word, board);
-                    if (isPossible) return true;
+                    if (backtracking(i, j, 0, word, board)) return true;
                 }
             }
         }
@@ -24,25 +19,23 @@ class Solution {
         return false;
     }
 
-    public void backtracking(int x,
-                             int y,
-                             int index,
-                             String target,
-                             char[][] board) {
-        if (index == target.length()) {
-            isPossible = true;
-            return;
-        }
+    public boolean backtracking(int x,
+                                int y,
+                                int index,
+                                String target,
+                                char[][] board) {
 
-        if (x < 0 || y < 0 || x >= height || y >= width) return;
+        if (index == target.length()) return true;
+        if (x < 0 || y < 0 || x >= height || y >= width) return false;
+        if (board[x][y] != target.charAt(index)) return false;
 
-        if (board[x][y] == target.charAt(index)) {
-            board[x][y] ^= 256;
-            backtracking(x + 1, y, index + 1, target, board);
-            backtracking(x, y + 1, index + 1, target, board);
-            backtracking(x - 1, y, index + 1, target, board);
-            backtracking(x, y - 1, index + 1, target, board);
-            board[x][y] ^= 256;
-        }
+        board[x][y] ^= 256;
+        boolean isPossible = backtracking(x + 1, y, index + 1, target, board) ||
+                backtracking(x, y + 1, index + 1, target, board) ||
+                backtracking(x - 1, y, index + 1, target, board) ||
+                backtracking(x, y - 1, index + 1, target, board);
+        board[x][y] ^= 256;
+
+        return isPossible;
     }
 }
