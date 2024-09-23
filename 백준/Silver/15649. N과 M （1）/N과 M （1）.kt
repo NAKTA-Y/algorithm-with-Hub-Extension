7dd85882ca15476ef4bf62659d0006c8f4cbb88d
NAKTA <nakta0511@gmail.com>
2024-09-23 10:46:17 +0900

    [Silver III] Title: N과 M (1), Time: 288 ms, Memory: 52328 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -1,28 +1,38 @@
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
     val split = readLine().split(" ")
     val n = split[0].toInt()
     val l = split[1].toInt()
     val numArray = IntArray(n) {it+1}
     val visited = BooleanArray(n) {false}
+    val result = IntArray(l)
 
-    backtracking(numArray, visited, l, "", 0)
-}
+    fun backtracking(numArray: IntArray, visited: BooleanArray, length: Int, result: IntArray, count: Int) {
 
-fun backtracking(numArray: IntArray, visited: BooleanArray, length: Int, result: String, count: Int) {
+        if (count == length) {
+            result.forEach {
+                bw.write("$it ")
+            }
+            bw.newLine()
+            return
+        }
 
-    if (count == length) {
-        println(result.trim())
-        return
-    }
-
-    for (i in 0 until numArray.size) {
-        if (!visited[i]) {
-            visited[i] = true
-            backtracking(numArray, visited, length, "$result ${numArray[i]}", count+1)
-            visited[i] = false
+        for (i in numArray.indices) {
+            if (!visited[i]) {
+                visited[i] = true
+                result[count] = numArray[i]
+                backtracking(numArray, visited, length, result, count+1)
+                visited[i] = false
+            }
         }
     }
+
+    backtracking(numArray, visited, l, result, 0)
+    bw.flush()
+    bw.close()
 }
