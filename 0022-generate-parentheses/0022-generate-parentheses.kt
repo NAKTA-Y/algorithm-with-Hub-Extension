e869b43808a580c5009ce0c64d47858853393cf2
NAKTA <nakta0511@gmail.com>
2024-09-23 16:13:25 +0900

    Time: 200 ms (20.99%), Space: 41.1 MB (5.52%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.kt
+++ 0022-generate-parentheses/0022-generate-parentheses.kt
@@ -2,28 +2,28 @@ class Solution {
     val answer = ArrayList<String>()
 
     fun generateParenthesis(n: Int): List<String> {
-        val length = n * 2
-        backtracking("", length)
+        backtracking("", n, 0, 0)
         return answer
     }
 
-    fun backtracking(brackets: String, length: Int) {
-        if (brackets.length == length) {
+    fun backtracking(brackets: String, n: Int, left: Int, right: Int) {
+        if (left > n || right > n) return
+        if (brackets.length == n * 2) {
             if (isCorrectBracket(brackets)) {
                 answer.add(brackets)
             }
             return
         }
 
-        backtracking("$brackets(", length)
-        backtracking("$brackets)", length)
+        backtracking("$brackets(", n, left + 1, right)
+        backtracking("$brackets)", n, left, right + 1)
     }
 
     fun isCorrectBracket(brackets: String): Boolean {
         val stack = ArrayDeque<Char>()
 
         for (bracket in brackets) {
-            if (bracket == ')'){
+            if (bracket == ')') {
                 if (stack.isEmpty())
                     return false
 
